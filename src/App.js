e5c06973f405da81913285997aae9460b066b61b
badari <badari0793@gmail.com>
2019-04-23 18:57:06 +0530

    adding bootstrap

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component} from 'react';
+import Contact from './components/Contact';
+import Header from './components/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+    return ( 
+      <div className = "App" >
+      <Header branding="Contact Manager"/>
+      <div className="container">
+       <Contact name="john Doe" Email="jdoe@gmail.com" phone="555-555-555"/>
+      <Contact name = "grame smith"
+            Email = "smith@gmail.com"
+            phone = "333-333-333" />
+
       </div>
+     
+       </div>
     );
   }
 }
