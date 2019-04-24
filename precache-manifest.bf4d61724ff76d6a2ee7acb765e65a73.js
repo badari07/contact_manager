0c71640d79e448c3e992495483d9beec4aa64958
badari <badari0793@gmail.com>
2019-04-25 02:58:13 +0530

    Updates

---
+++ precache-manifest.bf4d61724ff76d6a2ee7acb765e65a73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "54aec755285ee403ef2c",
+    "url": "/static/js/main.e8588b1a.chunk.js"
+  },
+  {
+    "revision": "1b3c03f9ef58b9b5fda2",
+    "url": "/static/js/2.1221da58.chunk.js"
+  },
+  {
+    "revision": "54aec755285ee403ef2c",
+    "url": "/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "1b3c03f9ef58b9b5fda2",
+    "url": "/static/css/2.ad4419f8.chunk.css"
+  },
+  {
+    "revision": "498b4641cddc9e3deb8d08114cc04180",
+    "url": "/index.html"
+  }
+];
