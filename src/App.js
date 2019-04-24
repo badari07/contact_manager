f40718f23dae5602fa47b1de41478aaddd803aca
badari <badari0793@gmail.com>
2019-04-24 19:58:51 +0530

    link setup

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
 import Contacts from './components/contacts/Contacts';
 import Header from './components/layout/Header';
 import About from './components/pages/About';
+import NotFoundPage from './components/pages/NotFoundPage';
 import AddContact from './components/contacts/AddContact';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
@@ -21,6 +22,7 @@ class App extends Component {
         <Route exact path='/' component={Contacts}/>
         <Route exact path='/contact/add' component={AddContact}/>
         <Route exact path='/about' component={About}/>
+        <Route component={NotFoundPage}/>
       </Switch>
       </div>
        </div>
--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -53,6 +53,8 @@ class AddContact extends Component {
            errors:{}
        });
 
+       this.props.history.push('/');
+
     }
   render() {
 
